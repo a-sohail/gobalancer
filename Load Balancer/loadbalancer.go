de6f851bf039d96f300d8320e439f4b6647bf244
Abdullah Sohail <asohail@surveymonkey.com>
2019-06-24 18:19:31 -0400

    Added git ignore

--- Load Balancer/loadbalancer.go
+++ Load Balancer/loadbalancer.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "You just got load balanced! %s", r.URL)
+	url := "localhost:7070"
+	http.Redirect(w, r, url, http.StatusSeeOther)
 }
 
 func main() {
